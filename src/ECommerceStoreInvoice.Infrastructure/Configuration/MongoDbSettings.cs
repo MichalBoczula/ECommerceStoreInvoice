3c9ef77a310b20ee01957c9ed61e41a1b5f24f1e
Michal  Boczula <38834900+MichalBoczula@users.noreply.github.com>
2026-03-31 09:58:19 +0200

    Implement order repository create/get/update persistence

--- src/ECommerceStoreInvoice.Infrastructure/Configuration/MongoDbSettings.cs
+++ src/ECommerceStoreInvoice.Infrastructure/Configuration/MongoDbSettings.cs
@@ -6,5 +6,6 @@
         public required string ConnectionString { get; init; }
         public required string DatabaseName { get; init; }
         public required string ShoppingCartsCollectionName { get; init; }
+        public required string OrdersCollectionName { get; init; }
     }
 }
---
+++ src/ECommerceStoreInvoice.Infrastructure/Persistence/Orders/OrderDocument.cs
@@ -0,0 +1,23 @@
+﻿using ECommerceStoreInvoice.Domain.AggregatesModel.Common.Enums;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ECommerceStoreInvoice.Infrastructure.Persistence.Orders
+{
+    internal sealed record OrderDocument
+    {
+        [BsonId]
+        [BsonGuidRepresentation(GuidRepresentation.Standard)]
+        public required Guid Id { get; init; }
+
+        [BsonGuidRepresentation(GuidRepresentation.Standard)]
+        public required Guid ClientId { get; init; }
+
+        public required IReadOnlyCollection<OrderLineDocument> Lines { get; init; }
+        public required DateTime CreatedAt { get; init; }
+        public required DateTime UpdatedAt { get; init; }
+        public required OrderStatus Status { get; init; }
+        public required decimal TotalAmount { get; init; }
+        public required string TotalCurrency { get; init; }
+    }
+}
