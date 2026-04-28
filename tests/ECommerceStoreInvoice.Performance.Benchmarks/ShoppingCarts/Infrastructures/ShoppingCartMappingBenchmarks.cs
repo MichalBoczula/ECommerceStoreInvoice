cc9516d30c7a9664195c1f674dc966774c2b62f6
Michal Boczula <michal.boczula0101@gmail.com>
2026-04-29 01:06:13 +0200

    Added more benchamrk tests

--- tests/ECommerceStoreInvoice.Performance.Benchmarks/ShoppingCarts/Infrastructures/ShoppingCartMappingBenchmarks.cs
+++ tests/ECommerceStoreInvoice.Performance.Benchmarks/ShoppingCarts/Infrastructures/ShoppingCartMappingBenchmarks.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
+using ECommerceStoreInvoice.Domain.AggregatesModel.ShoppingCartAggregate.ValueObjects;
 using ECommerceStoreInvoice.Infrastructure.Mapping;
 using ECommerceStoreInvoice.Infrastructure.Persistence.ShoppingCarts;
 using ECommerceStoreInvoice.Performance.Benchmarks.ShoppingCarts.Infrastructures.Common;
@@ -12,16 +13,32 @@ public class ShoppingCartMappingBenchmarks
     public int LinesCount { get; set; }
 
     private ShoppingCartDocument _document = null!;
+    private ShoppingCart _domain = null!;
+    private ShoppingCartLine _line = null!;
 
     [GlobalSetup]
     public void Setup()
     {
         _document = ShoppingCartDocumentBenchmarkDataFactory.CreateWithLines(LinesCount);
+        _domain = ShoppingCartMapping.MapToDomain(_document);
+        _line = _domain.Lines.First();
     }
 
     [Benchmark]
     public ShoppingCart MapDocumentToDomain()
     {
         return ShoppingCartMapping.MapToDomain(_document);
+    }
+
+    [Benchmark]
+    public object MapDomainToDocument()
+    {
+        return ShoppingCartMapping.MapToDocument(_domain);
+    }
+
+    [Benchmark]
+    public object MapLineToDocument()
+    {
+        return ShoppingCartMapping.MapLineToDocument(_line);
     }
 }
