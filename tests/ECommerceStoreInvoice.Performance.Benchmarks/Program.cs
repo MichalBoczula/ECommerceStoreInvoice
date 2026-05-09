a3cbcf59cbcdf81ee582b1e694508f0fcffe247f
Michal Boczula <michal.boczula0101@gmail.com>
2026-05-09 22:21:14 +0200

    Run all benchamrk tests

--- tests/ECommerceStoreInvoice.Performance.Benchmarks/Program.cs
+++ tests/ECommerceStoreInvoice.Performance.Benchmarks/Program.cs
@@ -1,12 +1,42 @@
 using BenchmarkDotNet.Running;
+using ECommerceStoreInvoice.Performance.Benchmarks.Application.Mapping;
+using ECommerceStoreInvoice.Performance.Benchmarks.ClientDataVersions.Application;
+using ECommerceStoreInvoice.Performance.Benchmarks.ClientDataVersions.Infrastructures;
+using ECommerceStoreInvoice.Performance.Benchmarks.Invoices.Application;
+using ECommerceStoreInvoice.Performance.Benchmarks.Invoices.Infrastructures;
+using ECommerceStoreInvoice.Performance.Benchmarks.Orders.Application;
+using ECommerceStoreInvoice.Performance.Benchmarks.Orders.Infrastructures;
+using ECommerceStoreInvoice.Performance.Benchmarks.ProductVersions.Infrastructures;
+using ECommerceStoreInvoice.Performance.Benchmarks.ShoppingCarts.Application;
+using ECommerceStoreInvoice.Performance.Benchmarks.ShoppingCarts.Infrastructures;
 
-namespace ECommerceStoreInvoice.Performance.Benchmarks
+namespace ECommerceStoreInvoice.Performance.Benchmarks;
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
-        {
-            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-        }
+        // General application mapping benchmarks
+        BenchmarkRunner.Run<MappingConfigBenchmarks>();
+
+        // ShoppingCarts
+        BenchmarkRunner.Run<ShoppingCartMappingConfigBenchmarks>();
+        BenchmarkRunner.Run<ShoppingCartMappingBenchmarks>();
+        BenchmarkRunner.Run<ShoppingCartRepositoryBenchmarks>();
+
+        // Orders
+        BenchmarkRunner.Run<OrderMappingConfigBenchmarks>();
+        BenchmarkRunner.Run<OrderMappingBenchmarks>();
+
+        // Invoices
+        BenchmarkRunner.Run<InvoiceMappingConfigBenchmarks>();
+        BenchmarkRunner.Run<InvoiceMappingBenchmarks>();
+
+        // ClientDataVersions
+        BenchmarkRunner.Run<ClientDataVersionMappingConfigBenchmarks>();
+        BenchmarkRunner.Run<ClientDataVersionMappingBenchmarks>();
+
+        // ProductVersions
+        BenchmarkRunner.Run<ProductVersionMappingBenchmarks>();
     }
 }
