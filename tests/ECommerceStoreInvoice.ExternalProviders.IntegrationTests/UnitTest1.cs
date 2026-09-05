93f7218ade6e69a9dcf7913e564bab71eeb5f336
Michal Boczula <michal.boczula0101@gmail.com>
2026-09-05 16:16:53 +0200

    test: add integration tests project for external providers
    Add the ECommerceStoreInvoice.ExternalProviders.IntegrationTests project to the solution and configure it with necessary test packages (xUnit, Shouldly, Moq, Testcontainers) and project references to test external integrations effectively.

---
+++ tests/ECommerceStoreInvoice.ExternalProviders.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Shouldly;
+
+namespace ECommerceStoreInvoice.ExternalProviders.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var test = true;
+        test.ShouldBeTrue();
+    }
+}
